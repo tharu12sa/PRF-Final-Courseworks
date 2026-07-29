287d0065c8b7d5f653ac92d6391938df8d697b47
tharu12sa <tharushakavinda621@gmail.com>
2026-07-30 00:08:24 +0530

    Enhance phone number validation with retry option and improved messaging

--- Finaltest.java
+++ Finaltest.java
@@ -11,8 +11,16 @@ import java.util.*;
         String phonenumber = input.nextLine();
 		String regex = "^07[0-1,2,4,5,6,7,8]\\d{7}$";
 		if (!phonenumber.matches(regex)) {
-            System.out.println("InValid  Phone Number");
-            PhoneNumber();
+            System.out.println("\t\tInValid  Phone Number");
+            System.out.print("Do you want add phone number again (Y/N) : ");
+            char answer = input.next().charAt(0);;
+            if(answer == 'Y' || answer == 'y'){
+                PhoneNumber();
+            }else{
+                printMenu();
+            }
+
+            
         } 
 	}
 	 public static void printContact(){
